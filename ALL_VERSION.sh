f3d6ee2b24f432a07001829c29289948ac1aecf6
Waim908 <waimxj449@foxmail.com>
2025-09-15 22:39:17 +0800

    test

---
+++ ALL_VERSION.sh
@@ -0,0 +1,10 @@
+export allVersion=(
+dxvk-gplasync-v2.1-1.tar.gz  dxvk-gplasync-v2.2-4.tar.gz    dxvk-gplasync-v2.5-1.tar.gz
+dxvk-gplasync-v2.1-2.tar.gz  dxvk-gplasync-v2.3.1-1.tar.gz  dxvk-gplasync-v2.5.2-1.tar.gz
+dxvk-gplasync-v2.1-3.tar.gz  dxvk-gplasync-v2.3-1.tar.gz    dxvk-gplasync-v2.5.3-1.tar.gz
+dxvk-gplasync-v2.1-4.tar.gz  dxvk-gplasync-v2.4.1-1.tar.gz  dxvk-gplasync-v2.6.1-1.tar.gz
+dxvk-gplasync-v2.2-1.tar.gz  dxvk-gplasync-v2.4-1.tar.gz    dxvk-gplasync-v2.6-1.tar.gz
+dxvk-gplasync-v2.2-2.tar.gz  dxvk-gplasync-v2.5.1-1.tar.gz  dxvk-gplasync-v2.6.2-1.tar.gz
+dxvk-gplasync-v2.2-3.tar.gz  dxvk-gplasync-v2.5.1-2.tar.gz
+)
+#  dxvk-gplasync-v2.7-1.tar.gz
---
+++ README.md
@@ -0,0 +1,3 @@
+# dxvk-gplasync 到 github 的镜像，通过github action 自动更新
+# Thanks
+[Ph42oN/dxvk-gplasync](https://gitlab.com/Ph42oN/dxvk-gplasync)
