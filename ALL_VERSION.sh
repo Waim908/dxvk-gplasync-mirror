a594fb54d018c5b45651ac0eb7f8fa3f207224fb
Waim908 <waimxj449@foxmail.com>
2025-09-16 13:18:47 +0800

    test

--- ALL_VERSION.sh
+++ ALL_VERSION.sh
@@ -1,4 +1,5 @@
-export allVersion=(
+# 例子
+allVersion=(
 dxvk-gplasync-v2.1-1.tar.gz  dxvk-gplasync-v2.2-4.tar.gz    dxvk-gplasync-v2.5-1.tar.gz
 dxvk-gplasync-v2.1-2.tar.gz  dxvk-gplasync-v2.3.1-1.tar.gz  dxvk-gplasync-v2.5.2-1.tar.gz
 dxvk-gplasync-v2.1-3.tar.gz  dxvk-gplasync-v2.3-1.tar.gz    dxvk-gplasync-v2.5.3-1.tar.gz
